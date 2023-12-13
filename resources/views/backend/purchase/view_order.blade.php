440f19e3eabd3be51c0cc224245981c6c3338305
JV <preubecitas@gmail.com>
2023-12-13 10:59:02 -0600

    [add] Se completo la vista de orden de compra

--- resources/views/backend/purchase/view_order.blade.php
+++ resources/views/backend/purchase/view_order.blade.php
@@ -19,12 +19,46 @@
                     </div>
                 </div>
             </div>
-
             <!-- end page title -->
+
             <div class="row">
                 <div class="col-lg-8 col-xl-12">
                     <div class="card">
                         <div class="card-body">
+                            <div class="tab-pane" id="settings">
+
+                                <!-- ORDER HEADER -->
+                                <div class="row">
+
+                                    <div class="col-md-6 mb-3">
+                                        <x-input-label :value="__('supplier')" class="form-label" />
+                                        <x-text-input class="form-control" type="text" :value="$order->supplier->name" readonly />
+                                    </div>
+
+                                    <div class="col-md-6 mb-3">
+                                        <x-input-label :value="__('code')" class="form-label" />
+                                        <x-text-input class="form-control" type="text" :value="$order->invoice_no" readonly />
+                                    </div>
+
+                                    <div class="col-md-6 mb-3">
+                                        <x-input-label :value="__('created-at')" class="form-label" />
+                                        <x-text-input class="form-control" type="text" :value="$order->created_at" readonly />
+                                    </div>
+
+                                    <div class="col-md-6 mb-3">
+                                        <x-input-label :value="__('date')" class="form-label" />
+                                        <x-text-input class="form-control" type="text" :value="$order->order_date" readonly />
+                                    </div>
+
+                                    <div class="col-md-12 mb-6">
+                                        <x-input-label :value="__('total')" class="form-label" />
+                                        <x-text-input class="form-control" type="text" :value="'Q ' . $order->total" readonly />
+                                    </div>
+
+                                </div> <!-- end row -->
+                            </div>
+                            <!-- end settings content-->
+
                             <div class="col-12">
                                 <div class="card">
                                     <div class="card-body">
